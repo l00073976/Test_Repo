07a4e34e5ed371ee2da9d15bebd3f2a1245e08df
l00073976 <l00073976@student.lyit.ie>
2018-10-02 19:39:15 +0100

    added Hellolyit

---
+++ HelloLYIT.java
@@ -0,0 +1,6 @@
+public class HelloLYIT{
+	public static void main(String[] args){
+		// Prints "Hello, LYIT from Wes l00073096" as output
+	System.out.println("Hello, LYIT from Wes l00073976");
+	}
+}
